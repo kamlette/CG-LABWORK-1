4dd240a3725aabebc8bddbd8cf2b80c9458c31a6
Kamilla <khamidulinareal@gmail.com>
2022-04-02 16:24:48 +0500

    lesson 3

--- CG LABWORK 1.cpp
+++ CG LABWORK 1.cpp
@@ -14,7 +14,7 @@ static void RenderSceneCB()
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-	glDrawArrays(GL_POINTS, 0, 1);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDisableVertexAttribArray(0);
 
 	glutSwapBuffers();
@@ -39,8 +39,11 @@ int main(int argc, char* argv[])
 		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
 		return 1;
 	}
-	glm::vec3 Vertices[1];
-	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+
+	glm::vec3 Vertices[3];
+	Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
+	Vertices[1] = glm::vec3(1.0f, -1.0f, 0.0f);
+	Vertices[2] = glm::vec3(0.0f, 1.0f, 0.0f);
 	
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
