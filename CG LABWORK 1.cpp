d15cfff0aeaec30260924a9857f218d44589f7fe
Kamilla <khamidulinareal@gmail.com>
2022-04-02 15:55:41 +0500

    lesson 2

--- CG LABWORK 1.cpp
+++ CG LABWORK 1.cpp
@@ -1,24 +1,51 @@
-﻿
+﻿#include <GL/glew.h>
 #include <iostream>
 #include <GL/freeglut.h>
+#include <glm/vector_relational.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec2.hpp>
+
+GLuint VBO;
 
 static void RenderSceneCB()
 {
-   glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glDrawArrays(GL_POINTS, 0, 1);
+	glDisableVertexAttribArray(0);
+
+	glutSwapBuffers();
 }
 
 int main(int argc, char* argv[])
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(1024, 768);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Tutorial 01");
-    glutDisplayFunc(RenderSceneCB);
-    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glutMainLoop();
-    glClear(GL_COLOR_BUFFER_BIT);
-    glutSwapBuffers();
-    return 0;
-} 
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1024, 768);
+	glutInitWindowPosition(100, 100);
+	glutCreateWindow("Tutorial 01");
+	glutDisplayFunc(RenderSceneCB);
+	glClearColor(0, 0, 0, 0);
+	
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+
+	GLenum res = glewInit();
+	if (res != GLEW_OK)
+	{
+		fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+		return 1;
+	}
+	glm::vec3 Vertices[1];
+	Vertices[0] = glm::vec3(0.0f, 0.0f, 0.0f);
+	
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+	
+	glutMainLoop();
+	return 0;
+}
